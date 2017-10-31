01ae24769fc7bbf3f53d53c578138c36f4dade89
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-10-31 09:41:29 +0300

    PHPUnit upgraded

--- tests/MemcachedCacheTest.php
+++ tests/MemcachedCacheTest.php
@@ -3,8 +3,9 @@ namespace tests;
 
 use Soupmix\Cache as c;
 Use Memcached;
+use PHPUnit\Framework\TestCase;
 
-class MemcachedCacheTest extends \PHPUnit_Framework_TestCase
+class MemcachedCacheTest extends TestCase
 {
     /**
      * @var \Soupmix\Cache\MemcachedCache $client
